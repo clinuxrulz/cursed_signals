fc2b6b21feaa4bc0d421f150b570d0f781e1bf1f
Clinton Selke <clinuxrulz@gmail.com>
2025-08-10 07:11:37 +1000

    initial commit

---
+++ src/test/java/io/github/clinuxrulz/cursed_signals/CSTest.java
@@ -0,0 +1,19 @@
+package io.github.clinuxrulz.cursed_signals;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CSTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
