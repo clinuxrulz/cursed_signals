fc02eaaf310f8e5e1e9b108b1d197d1b5a1adf7d
Clinton Selke <clinuxrulz@gmail.com>
2025-08-10 10:29:37 +1000

    start of jfx backend

---
+++ jfx_backend/src/main/io/github/clinuxrulz/cursed_signals_jfx_backend/JfxHyperStrategy.java
@@ -0,0 +1,10 @@
+package io.github.clinuxrulz.cursed_signals_jfx_backend;
+
+import io.github.clinuxrulz.cursed_signals.HyperStrategy;
+import io.github.clinuxrulz.cursed_signals.CS.Accessor;
+
+public class JfxHyperStrategy implements HyperStrategy {
+    @Override
+    public void addChildren(Object parent, Accessor children) {
+    }
+}
